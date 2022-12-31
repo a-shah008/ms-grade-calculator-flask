b75ebfafbe14f1674480c3e79223325544af2635
a_shah008 <aaravshah.300@gmail.com>
2022-12-30 19:33:07 -0600

    restructured program

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config["SECRET_KEY"] = '3e7bf39c2f9a3470ee4d10cc96685b36'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-db = SQLAlchemy(app)
-
-from app import routes
--- run.py
+++
@@ -1,8 +0,0 @@
-from app import app, db
-
-db.drop_all()
-db.create_all()
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    
